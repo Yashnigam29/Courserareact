b86cf8f3c8c0b3313a1c6ad18449a70f2f3c0fc7
yash nigam <yashnigam104@gmail.com>
2020-09-11 17:01:03 +0530

    reactcomponent2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './components/Menucomponent';
+import { DISHES } from './shared/dishes'
+import Menu1 from './components/menucomponent1';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+     
+    this.state={
+      dishes: DISHES
+    }
+  }
+  render(){
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -12,9 +22,10 @@ function App() {
           <NavbarBrand href="/">Resornate  con fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes}/>
+      {/* <Menu1 /> */}
     </div>
   );
 }
-
+}
 export default App;
